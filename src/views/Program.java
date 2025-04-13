fe8005b72c07b895fe48288c3c1177898db3b95a
Fabri519 <fabrizzio.lopezroldan@alu.frt.utn.edu.ar>
2025-04-13 00:43:49 -0300

    Agrego Menu nuevo

--- src/views/Program.java
+++ src/views/Program.java
@@ -10,7 +10,7 @@ public class Program {
 
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        ListarAnimalesView view = new ListarAnimalesView();
+        Menu view = new Menu();
         view.setVisible(true);
     }
 }
